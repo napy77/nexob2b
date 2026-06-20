071c2732143a962392b169f6e098fe62eef52537
German Yovan <germanyovan@gmail.com>
2026-06-20 16:23:14 +0000

    feat: initial Medusa v2 setup

---
+++ apps/backend/medusa-config.ts
@@ -0,0 +1,25 @@
+import { loadEnv, defineConfig } from '@medusajs/framework/utils'
+
+loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+
+module.exports = defineConfig({
+  projectConfig: {
+    databaseUrl: process.env.DATABASE_URL,
+    http: {
+      storeCors: process.env.STORE_CORS!,
+      adminCors: process.env.ADMIN_CORS!,
+      authCors: process.env.AUTH_CORS!,
+      jwtSecret: process.env.JWT_SECRET,
+      cookieSecret: process.env.COOKIE_SECRET,
+    }
+  },
+  admin: {
+    vite: (config) => {
+      config.server = {
+        ...config.server,
+        allowedHosts: ['nexob2b.app', 'www.nexob2b.app'],
+      }
+      return config
+    }
+  }
+})
